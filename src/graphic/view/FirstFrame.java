f2ca3b402a576a69fc0931db9fe400d35bfe9104
Britton Perry <britton.perry@hotmail.com>
2016-11-01 09:11:06 -0600

    The window  now has color and buttons!

--- src/graphic/view/FirstFrame.java
+++ src/graphic/view/FirstFrame.java
@@ -6,19 +6,24 @@ import java.awt.Dimension;
 
 public class FirstFrame extends JFrame
 {
+	
 	private FirstGUIController baseController;
+	private FirstPanel appPanel;
+	
 	
 	public FirstFrame(FirstGUIController baseController)
 	{
 		super();
 		this.baseController = baseController;
-		
+		appPanel = new FirstPanel(baseController);
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
+		this.setContentPane(appPanel);
 		this.setSize(new Dimension(500, 500));
+		this.setTitle("window");
 		this.setVisible(true);
 	}
 }
--- src/graphic/view/FirstPanel.java
+++ src/graphic/view/FirstPanel.java
@@ -2,15 +2,48 @@ package graphic.view;
 
 import javax.swing.JPanel;
 import graphic.controller.FirstGUIController;
+import java.awt.Color;
+import javax.swing.JButton;
 
-public class FirstPanel extends JPanel 
-{ 
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class FirstPanel extends JPanel {
+
 	private FirstGUIController baseController;
-	
-	public FirstPanel(FirstGUIController baseController)
-	{
+	private JButton colorButton;
+	private JButton randomButton;
+
+	public FirstPanel(FirstGUIController baseController) {
 		super();
 		this.baseController = baseController;
+
+		colorButton = new JButton("coloUr");
+		randomButton = new JButton("im so random");
+
+		setupPanel();
+		setupLayout();
+		setupListeners();
 	}
 
+	private void setupPanel() {
+		this.setBackground(Color.CYAN);
+		this.add(colorButton);
+		this.add(randomButton);
+	}
+
+	private void setupLayout() {
+
+	}
+
+	private void setupListeners() {
+		colorButton.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent click) 
+			{
+				setBackground(Color.GRAY);
+			}
+		});
+
+	}
 }
